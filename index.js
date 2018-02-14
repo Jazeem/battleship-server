4454fed3ca81523d2c157da5b0a4f5798c26c71f
Jazeem <jazeem23@gmail.com>
2018-02-15 03:45:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var players = [];
+var ships = [];
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+	
+	if(players.length < 2){
+		players.push(socket.id);
+		if(players.length == 2)
+			io.sockets.emit('gamestarted', 'new');
+	}
+	else
+		socket.emit('gameover', 'roomfull');
+
+	socket.on('shiparranged', function(data){
+		console.log(data);
+		playerShip = [];
+		playerShip.push()
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
