38e41fcc0ee42ba1699ebb69dbdde25e2b48506d
Jazeem <jazeem23@gmail.com>
2018-02-15 03:48:56 +0530

    index added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var io = require('socket.io')(http);
 var players = [];
 var ships = [];
 
+app.get('/', function(req, res){
+	res.send('<h1>Hello world</h1>');
+});
+
 io.on('connection', function(socket){
 	console.log('a user connected');
 	
